cd7742a6da24a4c402b9ad4e61609a0f6785c4a5
matthias.bock <matthias.bock@hu-berlin.de>
2011-09-09 14:26:21 +0000

    FChronik simplified

--- demo.py
+++ demo.py
@@ -3,12 +3,12 @@
 
 import FChronik, Ahnenblatt
 
-weber = FChronik.ahn("FChronik/weber.ahn")
+weber = FChronik.ahn("FChronik/Beispiel.ahn", show=True)
 
-#print str(len(weber.datasets))+" datasets"
+print str(len(weber.datasets))+" datasets"
 
 
-Beispiel = Ahnenblatt.ahn("Ahnenblatt/Beispiele/Beispiel.ahn")
+#Beispiel = Ahnenblatt.ahn("Ahnenblatt/Beispiele/Beispiel.ahn")
 
 #for dataset in Beispiel.datasets:
 #	print dataset.fields
